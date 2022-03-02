6154a3305af33fc64ee272141821799859d8fbcd
Daniel Schmidt <danielmschmidt92@gmail.com>
2022-03-02 13:28:52 +0100

    start with SO, no idea how to do vector of sth that implements this trait

--- src/main.rs
+++ src/main.rs
@@ -100,6 +100,45 @@ struct TwitterResponse {
     data: Vec<TwitterResponseItem>,
 }
 
+#[derive(Debug, Deserialize)]
+struct StackOverflowQuestion {
+    is_answered: bool,
+    link: String,
+    title: String,
+    answer_count: i32,
+}
+
+impl Shareable for StackOverflowQuestion {
+    fn title(&self) -> String {
+        self.title.clone()
+    }
+    fn link(&self) -> String {
+        self.link.clone()
+    }
+    fn link_name(&self) -> String {
+        let state = if self.is_answered {
+            ":white_check_mark:"
+        } else if self.answer_count > 0 {
+            ":waiting-spin:"
+        } else {
+            ":question:"
+        };
+
+        format!("{} StackOverflow", state)
+    }
+}
+
+impl Cacheable for StackOverflowQuestion {
+    fn cache_key(&self) -> String {
+        format!("stack-overflow-{}", self.link)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct StackOverflowResponse {
+    items: Vec<StackOverflowQuestion>,
+}
+
 #[tokio::main]
 async fn main() {
     // initialize tracing
@@ -165,6 +204,28 @@ async fn fetch_twitter_api(token: String, query: String) -> Result<TwitterRespon
     Ok(resp)
 }
 
+async fn fetch_stackoverflow_api(query: String) -> Result<StackOverflowResponse, String> {
+    let url = format!(
+        "https://api.stackexchange.com/search/advanced?site=stackoverflow.com&q={}",
+        query
+    );
+    let resp = match reqwest::Client::new().get(url).send().await {
+        Ok(resp) => match resp.json::<StackOverflowResponse>().await {
+            Ok(json) => json,
+            Err(err) => {
+                info!("{}", err);
+                return Err(format!("{}", err));
+            }
+        },
+        Err(e) => {
+            info!("{}", e);
+            return Err(format!("{}", e));
+        }
+    };
+
+    Ok(resp)
+}
+
 fn filter_duplicate_twitter_items(
     cache: &LocalCacheAccessor,
     resp: TwitterResponse,
@@ -185,6 +246,26 @@ fn filter_duplicate_twitter_items(
     return items_to_post;
 }
 
+fn filter_duplicate_stack_overflow_items(
+    cache: &LocalCacheAccessor,
+    resp: StackOverflowResponse,
+) -> Vec<StackOverflowQuestion> {
+    let mut items_to_post: Vec<StackOverflowQuestion> = Vec::new();
+    let mut c = cache.write().unwrap();
+
+    for item in resp.items {
+        if c.contains(item.cache_key()) {
+            info!("{} already posted", item.cache_key());
+        } else if item.never_share() {
+            info!("{} should not be shared", item.link());
+        } else {
+            items_to_post.push(item);
+            c.add(item.cache_key());
+        }
+    }
+    return items_to_post;
+}
+
 // basic handler that responds with a static string
 #[tracing::instrument]
 async fn root(
@@ -192,7 +273,7 @@ async fn root(
     Extension(cache): Extension<LocalCacheAccessor>,
 ) -> impl IntoResponse {
     let mut items_to_post: Vec<TwitterResponseItem> = Vec::new();
-    let twitter = tokio::join!(
+    let reponses = tokio::join!(
         fetch_twitter_api(
             config.twitter_api_bearer.clone(),
             format!("{}", config.keyword)
@@ -200,10 +281,11 @@ async fn root(
         fetch_twitter_api(
             config.twitter_api_bearer.clone(),
             format!("%23{}", config.keyword)
-        )
+        ),
+        fetch_stackoverflow_api(format!("{}", config.keyword))
     );
 
-    for item in [twitter.0, twitter.1] {
+    for item in [reponses.0, reponses.1] {
         match item {
             Ok(resp) => {
                 items_to_post.extend(filter_duplicate_twitter_items(&cache, resp));
@@ -211,6 +293,15 @@ async fn root(
             Err(err) => {
                 warn!("{}", err);
             }
+        }
+    }
+
+    match reponses.2 {
+        Ok(resp) => {
+            items_to_post.extend(filter_duplicate_stack_overflow_items(&cache, resp));
+        }
+        Err(err) => {
+            warn!("{}", err);
         }
     }
 
